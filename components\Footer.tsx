bf8b971521622e84275d25de7c8d40e1b626eef4
losas591-lab <losas591@gmail.com>
2026-06-04 09:21:42 -1100

    Create components\Footer.tsx

---
+++ components\Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer style={{
+      padding: "1rem",
+      marginTop: "3rem",
+      borderTop: "1px solid #333",
+      textAlign: "center"
+    }}>
+      <p>Built by Destiny • Inspired by SpaceX</p>
+    </footer>
+  );
+}
