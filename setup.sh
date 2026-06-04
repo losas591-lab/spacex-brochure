ab0bbedcbeb610737abcda864b7fe6e8d61969f0
losas591-lab <losas591@gmail.com>
2026-06-04 11:48:06 -1100

    Add setup script for one-command clone and run

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Clone the repository
+git clone https://github.com/losas591-lab/spacex-brochure.git
+
+# Navigate to the project directory
+cd spacex-brochure
+
+# Install dependencies
+npm install
+
+# Start the development server
+npm run dev
