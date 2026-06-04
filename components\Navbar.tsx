6067cc6d76199c78454ceeb1e90c7782ae825ba1
losas591-lab <losas591@gmail.com>
2026-06-04 09:21:14 -1100

    Create components\Navbar.tsx

---
+++ components\Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <nav style={{
+      padding: "1rem",
+      background: "#111",
+      borderBottom: "1px solid #333"
+    }}>
+      <h2>SpaceX Dashboard</h2>
+    </nav>
+  );
+}
